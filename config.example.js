db745438df7a40f65bbca8524137b22f4a877d52
your_username <ivy00johns@users.noreply.github.com>
2023-12-22 19:58:54 -0600

    - Updating config file. - Moving configs to seperate file. - Removing unused configs.

--- config.example.js
+++ config.example.js
@@ -39,25 +39,5 @@ module.exports = {
 	// Hashcat script constants
 	HASH_TYPE: 22000, // Hash type for Hashcat
 	ABORT_TEMPERATURE: 100, // Abort temperature for Hashcat
-	ABORT_WAIT_TIME: 2, // Abort wait time for Hashcat
-
-	// Hashcat file paths
-	HASHCAT_ATTACK_LISTS: "./hashcat/attacks-list/attacks-list.js", // Path to Hashcat attacks list
-	HASHCAT_ATTACK_SCRIPTS: "./hashcat/attack-scripts", // Path to Hashcat attack scripts
-
-	// General configurations
-	PRINT_ITEMS: 10, // Number of items to print in the terminal
-	GENERATE_PERMUTATIONS: 2000, // Number of permutations to generate and add to the .txt file
-	EXPORT_FILE_NAME: "./hashcat/wordlists/generated-passwords.txt", // Name of the exported file
-	WORD_LIST: [], // List of words for generation
-	MAX_WORDS_USED: 6, // Max number of words that can be combined to form a given string
-
-	// Sorting configurations
-	SORT_BY_LENGTH: false, // Whether to sort permutations by length
-	SORT_LENGTH_ASCENDING: false, // If sorting by length, whether to sort in ascending order
-	SORT_ALPHABETICALLY: true, // Whether to sort alphabetically
-
-	// Length constraints for generated strings
-	MIN_LENGTH: 8, // Minimum length of generated strings
-	MAX_LENGTH: 25, // Maximum length of generated strings
+	ABORT_WAIT_TIME: 2 // Abort wait time for Hashcat
 };
